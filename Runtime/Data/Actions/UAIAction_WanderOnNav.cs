e84e4354543ca024c9c7f2b84be1d4d43d6bf88c
Unnar <unnarvc@speedymail.org>
2026-07-29 14:25:38 +0000

    Improve nav wander action

--- Runtime/Data/Actions/UAIAction_WanderOnNav.cs
+++ Runtime/Data/Actions/UAIAction_WanderOnNav.cs
@@ -50,14 +50,16 @@ namespace Smidgenomics.Unity.UAI
 			return Time.time >= _interruptThreshold;
 		}
 
-		[SerializeField, Min(0.01f)] internal float _cooldown = 1f;
-		[SerializeField, Min(0.01f)] internal float _cancelledCooldown = 10f;
+		[SerializeField, Min(0f)] internal float _cooldown = 1f;
+		[SerializeField, Min(0f)] internal float _cancelledCooldown = 10f;
 		[SerializeField, Min(0.01f)] internal float _interruptibleAfter = 1f;
 		[SerializeField, Min(0f)] internal float _deactivationDuration;
 		
 		[Header("Navigation")]
 		[SerializeField, Min(0.5f)] private float _wanderRadius = 10;
 		[SerializeField, Min(0.5f)] private float _maxSampleDistance = 2f;
+		[SerializeField, Range(5f, 45f)] private float _wanderTurnAngle = 15f;
+		[SerializeField, Range(1, 10)] private float _sampleRetries = 6;
 
 		[Min(0.05f)]
 		[SerializeField] private float _stopThreshold = 0.3f;
@@ -76,21 +78,35 @@ namespace Smidgenomics.Unity.UAI
 
 		private Vector3 GetRandomDestination(NavMeshAgent agent)
 		{
-			var roffset = Random.insideUnitSphere * _wanderRadius;
+			var randomDir = agent.transform.forward;
 
-			var sample = agent.transform.position + roffset;
+			var turnAngle = _wanderTurnAngle;
+			var retryStep = (360f - turnAngle) / _sampleRetries;
 
 			NavMeshQueryFilter filter = new()
 			{
-				agentTypeID = agent.agentTypeID
+				agentTypeID = agent.agentTypeID,
+				areaMask = agent.areaMask
 			};
-			
-			if (NavMesh.SamplePosition(sample, out NavMeshHit hit, _maxSampleDistance, filter))
+
+			var randSign = Random.value > 0.5 ? 1 : -1;
+			var randAngle = Random.Range(0f, turnAngle) * randSign;
+			var wanderDist = Random.Range(_wanderRadius * 0.5f, _wanderRadius);
+
+			for (int i = 0; i < _sampleRetries; i++)
 			{
-				return hit.position;
+				var angle = randAngle + (i * retryStep * randSign);
+				var q = Quaternion.Euler(Vector3.up * angle);
+				var wDir = q * randomDir;
+				var tPoint = agent.transform.position + wDir * wanderDist;
+
+				if (NavMesh.SamplePosition(tPoint, out NavMeshHit hit, _maxSampleDistance, filter))
+				{
+					return hit.position;
+				}
 			}
 
-			return sample;
+			return agent.transform.position;
 
 		}
 
